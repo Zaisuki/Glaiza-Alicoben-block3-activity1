a86cc0d0a6dfb6297e0c2eaabfec503ba2479013
Yurei <glba.alicoben.up@phinmaed.com>
2023-12-08 11:59:32 +0800

    input notes

--- app/src/main/java/com/alicoben/glaiza/block3/p1/quiz/Notes.kt
+++ app/src/main/java/com/alicoben/glaiza/block3/p1/quiz/Notes.kt
@@ -3,12 +3,9 @@ package com.alicoben.glaiza.block3.p1.quiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
-
-
+class Notes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_notes)
     }
 }
